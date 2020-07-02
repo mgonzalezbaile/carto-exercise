763c2d4ee55df8fa68a796cfb3dd059816f902f6
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2020-07-02 17:39:11 +0200

    Refactor /activities resource to use the file_activities_repository.

--- src/http_resources.py
+++ src/http_resources.py
@@ -1,63 +1,19 @@
 import json
-import os
-from dataclasses import dataclass
-from typing import List, Optional
 
 from flask import Response, Flask, request
-from toolz import pipe
-from toolz.curried import filter
+
+from src.activities_repository import FindActivitiesCriteria
+from src.file_activities_repository import fetch_activities_by_criteria_geojson
 
 app = Flask(__name__)
 
 
-@dataclass
-class FindActivitiesCriteria:
-    category: Optional[str] = None
-    location: Optional[str] = None
-    district: Optional[str] = None
-
-
 @app.route('/activities', methods=['GET'])
 def get_activities():
-    activities = read_file()
-    filtered_activities = pipe(
-        activities,
-        filter_by_criteria(request.args)
-    )
+    result = fetch_activities_by_criteria_geojson(FindActivitiesCriteria(
+        location=request.args.get('location', None),
+        district=request.args.get('district', None),
+        category=request.args.get('category', None),
+    ))
 
-    result = {"text": "hello World"}
-
-    return Response(response=json.dumps(activities), headers={'Content-Type': 'application/json'})
-
-
-def read_file() -> List[dict]:
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/madrid.json') as f:
-        data = json.load(f)
-
-        return data
-
-
-def find_activities_by_criteria(criteria: FindActivitiesCriteria):
-    activities = read_file()
-    filtered_activities = pipe(
-        activities,
-        filter_by_criteria(criteria)
-    )
-
-    return list(filtered_activities)
-
-
-def filter_by_criteria(criteria: FindActivitiesCriteria) -> callable:
-    return filter(
-        lambda activity:
-        filter_by_key(activity, 'category', criteria) and
-        filter_by_key(activity, 'location', criteria) and
-        filter_by_key(activity, 'district', criteria)
-    )
-
-
-def filter_by_key(activity, key, criteria: FindActivitiesCriteria):
-    if not getattr(criteria, key):
-        return True
-
-    return hasattr(criteria, key) and (activity[key] == getattr(criteria, key))
+    return Response(response=json.dumps(result), headers={'Content-Type': 'application/json'})

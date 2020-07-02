d27700d9f16b32fcd3b3b44535433f6ff5d3e2a7
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2020-07-02 15:27:46 +0200

    WIP: Find activities by Criteria. Filter by location working.

--- src/http_resources.py
+++ src/http_resources.py
@@ -1,12 +1,56 @@
 import json
+import os
+import pathlib
+from typing import List
 
-from flask import Response, Flask
+from flask import Response, Flask, request
+from toolz import pipe
+from toolz.curried import filter
 
 app = Flask(__name__)
 
 
 @app.route('/activities', methods=['GET'])
 def get_activities():
+    activities = read_file()
+    filtered_activities = pipe(
+        activities,
+        filter_by_query_params(request.args)
+    )
+
     result = {"text": "hello World"}
 
-    return Response(response=json.dumps(dict(result)), headers={'Content-Type': 'application/json'})
+    return Response(response=json.dumps(activities), headers={'Content-Type': 'application/json'})
+
+
+def read_file() -> List[dict]:
+    with open(os.path.dirname(os.path.realpath(__file__)) + '/madrid.json') as f:
+        data = json.load(f)
+
+        return data
+
+
+def find_activities_by_criteria(criteria):
+    activities = read_file()
+    filtered_activities = pipe(
+        activities,
+        filter_by_query_params(criteria)
+    )
+
+    return list(filtered_activities)
+
+
+def filter_by_query_params(query_params: dict) -> callable:
+    if not query_params:
+        return lambda activity: activity
+
+    return filter(
+        lambda activity:
+        filter_by_key(activity, 'category', query_params) or
+        filter_by_key(activity, 'location', query_params) or
+        filter_by_key(activity, 'district', query_params)
+    )
+
+
+def filter_by_key(activity, key, query_params):
+    return (key in query_params) and (activity[key] == query_params[key])
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,7 +1,28 @@
 from assertpy import assert_that
 
+from src.http_resources import find_activities_by_criteria
 from src.something import do_something
 
 
 def test_do_something():
     assert_that(do_something()).is_equal_to(False)
+
+
+def test_find_activities_by_location():
+    activities = find_activities_by_criteria({
+        "location": "outdoors"
+    })
+
+    for activity in activities:
+        assert_that(activity['location'] == 'outdoors').is_true()
+
+    assert_that(len(activities)).is_equal_to(7)
+
+    activities = find_activities_by_criteria({
+        "location": "indoors"
+    })
+
+    for activity in activities:
+        assert_that(activity['location'] == 'indoors').is_true()
+
+    assert_that(len(activities)).is_equal_to(3)
